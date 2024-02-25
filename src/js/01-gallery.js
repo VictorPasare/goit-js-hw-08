4beb04f1f317b02c4ca2ccbd32ed00849b93e6cf
VictorPasare <victor.pasare95@gmail.com>
2024-02-25 15:21:58 +0200

    Update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+const imagesMenuEl = document.querySelector('.gallery');
+imagesMenuEl.style.listStyle = 'none';
+
+function createImages(image) {
+  return image
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+</li>`
+    )
+    .join('');
+}
+
+imagesMenuEl.insertAdjacentHTML('beforeend', createImages(galleryItems));
+
+const lightbox = new SimpleLightbox('.gallery__link', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
